e4755c4057cb36764d3733b54aa0f6015dd43bf3
Sahraoui Mohammed <65463174+X-BITS@users.noreply.github.com>
2023-07-27 15:41:29 -0700

    Add files via upload

---
+++ programs/Weird pyramid/script.py
@@ -0,0 +1,17 @@
+number_blocks = int(input("input the number of blocks:"))
+i = 1
+h = 1
+t = i
+String = "[]"
+print(String, "    =========> rank 1")
+while i < number_blocks :
+    if (i + (t + 1)) > number_blocks:
+        break
+    t += 1
+    i += t
+    String += "[]"
+    h += 1
+    print(String, "    =========> rank", h)
+    
+
+print("The height of the pyramid:", h)
