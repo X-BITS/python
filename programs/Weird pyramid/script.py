e2d86dad3229fe3a04564aa056d88ebd70a176f1
Sahraoui Mohammed <65463174+X-BITS@users.noreply.github.com>
2023-07-27 17:08:27 -0700

    Update script.py

--- programs/Weird pyramid/script.py
+++ programs/Weird pyramid/script.py
@@ -1,3 +1,6 @@
+# version : python 3.9   
+# desc    : delete duplication from a given list
+# author  : Sahraoui Mohammed Taher Amine.
 number_blocks = int(input("input the number of blocks:"))
 i = 1
 h = 1
