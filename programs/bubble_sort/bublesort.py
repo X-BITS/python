cf0d473e0f313d66422abadb6291820d4156ef45
Sahraoui Mohammed <65463174+X-BITS@users.noreply.github.com>
2023-07-28 09:00:19 -0700

    Add files via upload

---
+++ programs/bubble_sort/bublesort.py
@@ -0,0 +1,21 @@
+# version : python 3.9   
+# desc    : sorting an array of numbers based on native way of human thinking, the bubble sort. 
+# author  : Sahraoui Mohammed Taher Amine.
+
+my_list = []
+swapped = True
+num = int(input("How many elements do you want to sort: "))
+
+for i in range(num):
+    val = float(input("Enter a list element: "))
+    my_list.append(val)
+
+while swapped:
+    swapped = False
+    for i in range(len(my_list) - 1):
+        if my_list[i] > my_list[i + 1]:
+            swapped = True
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+
+print("\nSorted:")
+print(my_list)
